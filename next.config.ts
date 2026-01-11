98dc70de6a713287d6bef653d7ab2e6cb4e8e96b
Kan13 <kan13@example.com>
2026-01-11 18:17:15 +0100

    Configure for static export to GitHub Pages

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   allowedDevOrigins: ['10.254.132.154'],
+  output: 'export',
+  trailingSlash: true,
 };
 
 export default nextConfig;
