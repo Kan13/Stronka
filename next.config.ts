f456f67beb2fc37112f609bc3b23c895dd8ca195
Kan13 <kan13@example.com>
2026-01-12 14:01:15 +0100

    Update baseline-browser-mapping and prepare for deployment

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   allowedDevOrigins: ['10.254.132.154'],
-  output: 'export',
-  trailingSlash: true,
-  basePath: '/Stronka',
 };
 
 export default nextConfig;
